60a74c3847f5c2b03ebd82795d6dfd63854e7123
Bumpversion after CI <bumpversion-after-ci@example.com>
2019-08-15 09:35:25 +0000

    Bump version: 2018.15.0 -> 2018.16.0 [ci skip]

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
 
     # Updated via travisd: https://travis-ci.org/guettli/dumpenv
     # See .travis.yml
-    version='2018.15.0',
+    version='2018.16.0',
 
     description='dumpenv: Dump values of the current Python environment',
     long_description=long_description,
